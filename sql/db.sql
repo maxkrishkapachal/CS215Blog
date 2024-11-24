02f2080dc63081578f157b5fe28b05184b8322c5
Max Krishka-Pachal <60788817+maxkrishkapachal@users.noreply.github.com>
2024-11-24 13:32:32 -0600

    create tables only if they don't exist

--- sql/db.sql
+++ sql/db.sql
@@ -1,9 +1,9 @@
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     user_id         INT             NOT NULL AUTO_INCREMENT,
-    email           VARCHAR(50)     NOT NULL,
     first_name      VARCHAR(50),    
-    last_name       VARCHAR(50),    
+    last_name       VARCHAR(50),  
+    email           VARCHAR(50)     NOT NULL,  
     username        VARCHAR(50)     NOT NULL,
     password        VARCHAR(50)     NOT NULL,
     dob             DATE            NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE users (
     PRIMARY KEY     (user_id)
 );
 
-CREATE TABLE post (
+CREATE TABLE IF NOT EXISTS post (
     post_id         INT             NOT NULL AUTO_INCREMENT,
     user_id         INT             NOT NULL,
     timestamp       TIMESTAMP       NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE post (
     FOREIGN KEY     (user_id) REFERENCES users (user_id)
 );
 
-CREATE TABLE comment (
+CREATE TABLE IF NOT EXISTS comment (
     comment_id      INT             NOT NULL AUTO_INCREMENT,
     post_id         INT             NOT NULL,
     user_id         INT             NOT NULL,
@@ -33,7 +33,7 @@ CREATE TABLE comment (
     FOREIGN KEY     (post_id) REFERENCES post (post_id)
 );
 
-CREATE TABLE postvote (
+CREATE TABLE IF NOT EXISTS postvote (
     post_vote_id    INT             NOT NULL AUTO_INCREMENT,
     post_id         INT             NOT NULL,
     user_id         INT             NOT NULL,
@@ -43,7 +43,7 @@ CREATE TABLE postvote (
     FOREIGN KEY     (post_id) REFERENCES post (post_id)
 );
 
-CREATE TABLE commentvote (
+CREATE TABLE IF NOT EXISTS commentvote (
     comment_vote_id INT             NOT NULL AUTO_INCREMENT,
     comment_id      INT             NOT NULL,
     user_id         INT             NOT NULL,
